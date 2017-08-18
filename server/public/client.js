5a3d41f75fa7238f3506d295d4e987e06716ab0f
Nic Bade <chipchapmaniii@gmail.com>
2017-08-18 16:39:19 -0500

    set up and connected angular to html / client.js

---
+++ server/public/client.js
@@ -0,0 +1,14 @@
+console.log('JS loaded');
+
+var app = angular.module('EmployeeApp', []);
+
+app.controller('EmployeeController', ['$http', function($http) {
+    console.log('Employee Controller has been loaded!');
+    var self = this;
+    self.employees = [];
+
+
+
+
+
+}]); // end of app.controller
